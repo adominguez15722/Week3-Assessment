a50d7de8e3e3bef8d88a991a4f6e31b992f53d3b
Anthony <adominguez15722@gmail.com>
2022-01-21 11:08:00 -0600

    Part 4 is done

---
+++ profile.js
@@ -0,0 +1,34 @@
+let colorBtn = document.querySelector('#color')
+
+function favColor()  {
+    alert('My favorite color is green')
+}
+
+colorBtn.addEventListener('click', favColor)
+
+
+
+
+let favBtn = document.querySelector('#place')
+
+function favPlace()  {
+
+    alert('My favorite place is Dallas')
+
+}
+
+favBtn.addEventListener('click', favPlace )
+
+
+
+
+
+let favRit = document.querySelector('#ritual')
+
+function favRitual()  {
+
+    alert('My favorite ritual is eating dinner with roommates')
+
+}
+
+favRit.addEventListener('click', favRitual)
