a8436e1109223946575823d75987ad5769cea7ad
Anthony <adominguez15722@gmail.com>
2022-01-21 09:37:53 -0600

    Part 2 is complete

--- about.js
+++ about.js
@@ -5,10 +5,21 @@ console.log("hello world");
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	console.log('form submit');
+	alert('Form has been submitted');
 }
 
 
 let form = document.querySelector('form#contact');
 
 form.addEventListener('submit', handleSubmit);
+
+
+function overImg(evt)	{
+	evt.preventDefault();
+
+	alert('What a great picture you picked!')
+}
+
+let duck = document.querySelector('img')
+
+duck.addEventListener('mouseover', overImg)
